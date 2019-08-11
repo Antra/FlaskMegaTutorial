4343a2f3bfcea63fd1d7c36369b4633f732900e1
Anders Demant van der Weide <antra@antra.dk>
2019-08-11 15:58:09 +0200

    Chapter 2: Templates

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,17 @@
+from flask import render_template
 from app import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello World!"
+    user = {'username': 'antra'}
+    posts = [{
+        'author': {'username': 'John'},
+        'body': 'Beautiful day in Portland'
+    },
+        {
+        'author': {'username': 'Susan'},
+        'body': 'The Avengers movie was so cool!'
+    }]
+    return render_template('index.html', title='Home', user=user, posts=posts)
