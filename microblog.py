373d596e63742c86fcd56ba31431f9313dc44635
Anders Demant van der Weide <antra@antra.dk>
2019-10-25 21:33:18 +0200

    Chapter 15: A better application structure

--- microblog.py
+++ microblog.py
@@ -1,14 +1,10 @@
-from app import app, db, cli
+from app import create_app, db, cli
 from app.models import User, Post
 
+app = create_app()
+cli.register(app)
 
 # decorator defining what will be available for 'flask shell' (NB, inside the venv!)
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db,
-            'User': User,
-            'Post': Post}
-
-
-if __name__ == '__main__':
-    app.run()
+    return {'db': db, 'User': User, 'Post': Post}
