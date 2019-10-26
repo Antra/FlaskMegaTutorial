91dc2bf4f744f2e828004947cc3e0cd8c84dcc17
Anders Demant van der Weide <antra@antra.dk>
2019-10-26 11:49:51 +0200

    Chapter 21: User notifications

--- microblog.py
+++ microblog.py
@@ -7,4 +7,4 @@ cli.register(app)
 # decorator defining what will be available for 'flask shell' (NB, inside the venv!)
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User, 'Post': Post}
+    return {'db': db, 'User': User, 'Post': Post, 'Message': Message, 'Notification': Notification}
